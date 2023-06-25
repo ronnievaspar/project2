26cdff20ab051c5a42cb147af11379779737caa0
rounakkumararya <rounakkumar040@.gmail>
2023-06-25 15:43:04 +0530

    few changes done

--- public/script.js
+++ public/script.js
@@ -1,28 +1,3 @@
-const activePage = window.location.pathname;
-const navlinks = document.querySelectorAll(".navlinks");
-
-navlinks.forEach((links) => {
-  if (links.href.includes(`${activePage}`)) {
-    links.classList.add("active");
-  }
-});
-
-const bar = document.getElementById("bar");
-const nav = document.getElementById("navbar");
-const close = document.getElementById("close");
-
-if (bar) {
-  bar.addEventListener("click", () => {
-    nav.classList.add("active");
-  });
-}
-
-if (close) {
-  close.addEventListener("click", () => {
-    nav.classList.remove("active");
-  });
-}
-
 function popup() {
   var blur = document.getElementById("blur");
   blur.classList.toggle("active");
